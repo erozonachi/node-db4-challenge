decef4c83dcda68accb0c810b893ca7ff63ed4c5
erozonachi <erozonachi.james@gmail.com>
2019-07-18 21:59:19 +0100

    Add get-shopping-list function to the recipe model

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -4,4 +4,11 @@ module.exports = {
   getRecipes: function() {
     return db('recipes');
   },
+
+  getShoppingList: function(recipe_id) {
+    return db('recipe-items')
+      .select('ing_name', 'quantity')
+      .join('ingredients', 'ing_id', 'ingredients.id')
+      .where({ recipe_id });
+  },
 };
