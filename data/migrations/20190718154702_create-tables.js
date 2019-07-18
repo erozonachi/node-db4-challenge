c6fd36066d550cafbe6e406137fdc2a276aeee2e
erozonachi <erozonachi.james@gmail.com>
2019-07-18 16:26:12 +0100

    Create database tables
    Create recipes, ingredients, steps and recipe-items tables.

--- data/migrations/20190718154702_create-tables.js
+++ data/migrations/20190718154702_create-tables.js
@@ -1,6 +1,55 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.text('recipe_title', 128)
+        .unique()
+        .notNullable();
+      tbl.text('recipe_desc');
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments();
+      tbl.text('ing_name', 128)
+        .unique()
+        .notNullable();
+      tbl.text('ing_unit', 64)
+        .notNullable();
+      tbl.decimal('ing_unit_price')
+        .notNullable();
+    })
+    .createTable('steps', tbl => {
+      tbl.increments();
+      tbl.integer('step_number')
+        .unsigned()
+        .notNullable();
+      tbl.text('instruction')
+        .notNullable();
+      tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('recipe-items', tbl => {
+      tbl.increments();
+      tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl.integer('ing_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
 };
 
 exports.down = function(knex) {
